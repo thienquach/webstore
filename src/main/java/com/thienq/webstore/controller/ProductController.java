a14529610b77655568d3be50c85422ad48e53c64
thien.quach <thien.quach@sai-digital.com>
2016-06-08 10:12:09 +0700

    update

--- src/main/java/com/thienq/webstore/controller/ProductController.java
+++ src/main/java/com/thienq/webstore/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.thienq.webstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
